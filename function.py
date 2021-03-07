431beefb3dae4e662806177eceeb33a65d29901c
Balakrishnan <69191050+bala876@users.noreply.github.com>
2021-03-07 19:24:52 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,46 @@
+#python function
+
+#A function is a block of code which only runs when it called.
+
+#a function can  return data as a result...
+
+#There are two types ...
+
+#def function...
+
+#lambda function...
+
+#creation a function
+
+#def index(request):
+    #return request "index.html" {} #sample code....
+
+#def my_function(): #create the function
+    #print("hello from a function")
+
+#my_function() # close function.....
+
+#def my_function(fname):
+    #print(fname + "bala")
+
+#my_function("hi")
+#my_function("ram")
+#my_function("bala")
+
+#two names...
+
+#def my_function(food):
+    #for x in food: #simple code....
+        #print(x)
+
+
+#book =['book1','book2','book3']
+
+#my_function(book)
+
+def my_function(x):
+    return 5 *x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(6))
