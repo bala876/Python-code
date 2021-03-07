bb5e38c176909c54051d3526374aba791d218ac2
Balakrishnan <69191050+bala876@users.noreply.github.com>
2021-03-07 19:24:04 +0530

    Add files via upload

---
+++ exception .py
@@ -0,0 +1,14 @@
+#exception handling
+
+try:
+    x =("bala",'2','3','4') #tuples add 
+    x.append('5')
+    print(x)
+    
+except:
+    list =["bala",'2','3','4'] #list add ...
+    list.append('5')
+    print(list)
+
+#else:
+    #print("nothing went wrong")
