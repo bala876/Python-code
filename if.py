dd1fc600a1b413ee4b0bb9913e673550e84fc340
Balakrishnan <69191050+bala876@users.noreply.github.com>
2021-03-07 19:25:35 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,12 @@
+#if -else
+#be careful with the indications in if else blocks
+
+#if condition
+
+a =5
+if a<1:
+    print("index.html")
+    
+else:
+    print("Please enter a correct username and password. Note that both fields may be case-sensitive.")
+
